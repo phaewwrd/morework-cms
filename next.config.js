000ee5a03a84762e925d6bc32d48d906f6e4bd6d
praew.morestudio <praew@morestudio.co.th>
2025-09-25 17:26:10 +0700

    Remove .next from repository and ignore it

--- next.config.js
+++ next.config.js
@@ -1,8 +1,34 @@
 /** @type {import('next').NextConfig} */
+const isDev = process.env.NODE_ENV === 'development'
+
 const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
-};
+  
+  compress: !isDev, 
+  
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          { key: 'X-XSS-Protection', value: '1; mode=block' }
+        ]
+      }
+    ]
+  },
 
-export default nextConfig;
+  env: {
+    NODE_ENV: process.env.NODE_ENV,
+  },
+
+  productionBrowserSourceMaps: isDev, // dev = true, prod = false
+
+  output: isDev ? undefined : 'standalone',
+}
+
+module.exports = nextConfig
