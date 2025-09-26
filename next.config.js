b8e307ef9dc94976a11aee4e5fa32b746bf79da3
praew.morestudio <praew@morestudio.co.th>
2025-09-26 10:10:25 +0700

    test middleware.ts

--- next.config.js
+++ next.config.js
@@ -1,34 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isDev = process.env.NODE_ENV === 'development'
-
 const nextConfig = {
   experimental: {
     typedRoutes: true,
   },
-  
-  compress: !isDev, 
-  
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          { key: 'X-XSS-Protection', value: '1; mode=block' }
-        ]
-      }
-    ]
-  },
+};
 
-  env: {
-    NODE_ENV: process.env.NODE_ENV,
-  },
-
-  productionBrowserSourceMaps: isDev, // dev = true, prod = false
-
-  output: isDev ? undefined : 'standalone',
-}
-
-module.exports = nextConfig
+export default nextConfig;
